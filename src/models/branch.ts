16e321e83036aaa90a7ef5eb9be2f6130779ad5e
vicstaples <victorhugo.barrios.jimenez@alumnos.upm.es>
2018-05-29 20:14:43 -0500

    adding branch details

--- src/models/branch.ts
+++ src/models/branch.ts
@@ -1,28 +1,29 @@
+//Branch reprresenta a cada Sucursal en las cuales se esta trabajando
 export interface Branch { 
-    uid?: string;
-    name?: string;
-    city?: string;
-    state?: string;
-    supervisorMin?: number;
-    promoters?: number;
-    supervisors?: number;
-    supervisorMax?: number;     
-    promotersMin?: number;
-    promotersMax?: number;           
-    address1?: string;     
-    address2?: string;
-    postalCode?: string;
-    contact?: string;
-    contactEmail?: string;     
-    scheduleMonFriOpen?: string; 
-    scheduleMonFriClose?: string;         
-    scheduleSatOpen?: string;
-    scheduleSatClose?: string;        
-    scheduleSunOpen?: string; 
-    scheduleSunClose?: string;      
-    details?: string;
-    image?: string;
-    extraHours?: number;
-    coordinatesLat?: number;
-    coordinatesLng?: number;
+    uid?: string; // Id de la sucursal
+    name?: string; // Nombre
+    city?: string; // Ciudad
+    state?: string; // Estado
+    supervisorMin?: number; //Numero minimo de supervisores
+    promoters?: number; // Numero de promotores
+    supervisors?: number; // Numero de supervisores 
+    supervisorMax?: number;  // Nunmero maximo de supervisores
+    promotersMin?: number; // Numero minimo de promotores
+    promotersMax?: number; // Numero máximo de promotores
+    address1?: string;  // Direccion no.1 
+    address2?: string; // Direccion no. 2 
+    postalCode?: string; // Codigo postal
+    contact?: string; // Telefono de contacto
+    contactEmail?: string; // Email de contacto de la sucursal 
+    scheduleMonFriOpen?: string; // Horario semanal de apertura
+    scheduleMonFriClose?: string; // Horario semanal de clausura
+    scheduleSatOpen?: string; //Horario de apertura sabados  
+    scheduleSatClose?: string; //Horario de clausura sabados 
+    scheduleSunOpen?: string; //Horario de apertura domingos
+    scheduleSunClose?: string; // Horario de apertura domingos
+    details?: string; //Detalles extras de la sucursal 
+    image?: string; //imagen de la sucursal 
+    extraHours?: number; //Numero de horas extra permitidas
+    coordinatesLat?: number; //Latitud de las coordenadas de la sucursal 
+    coordinatesLng?: number; // Longitud de las coordenadss de la sucursal 
 }
