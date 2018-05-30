9326e8f6ac52107739ef95f554dc255f4f3406c9
vicstaples <victorhugo.barrios.jimenez@alumnos.upm.es>
2018-05-29 20:09:04 -0500

    Adding details to event class

--- src/models/events.ts
+++ src/models/events.ts
@@ -1,10 +1,11 @@
+// Event simboliza un periodo laboral de un promotor en una sucursal
 export class Event { 
-    uid?: string;
-    branch?: string;
-    promoter?: string;
-    eventDateBegin?: Date;
-    eventDateExp?: Date;
-    status?: boolean;
-    hourWorkdayBegin?: string;
-    hourWorkdayEnd?: string;
+    uid?: string; //Id del evento 
+    branch?: string; // Id d ela sucursal 
+    promoter?: string; // Id del promotor 
+    eventDateBegin?: Date; // Fecha de inicio del periodo del evento
+    eventDateExp?: Date; //Fecha de fin del periodo  
+    status?: boolean; // Estado del evento
+    hourWorkdayBegin?: string; // Hora de entrada diaria de los eventos
+    hourWorkdayEnd?: string; // Hora de salida diaria de los eventos
 }
